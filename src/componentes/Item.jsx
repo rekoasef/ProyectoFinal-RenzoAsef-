ebdb25d9e2279fb405f82193b0c402ae34e84d3f
renzoasef <renzoasef02@gmail.com>
2024-01-29 16:00:59 -0300

    Se modificaron las imagenes

--- src/componentes/Item.jsx
+++ src/componentes/Item.jsx
@@ -1,11 +1,27 @@
 import { Link } from "react-router-dom";
+const Imagenes = ({item}) => {
+    const estiloComponente = {
+        padding: "20px",
+        width: "250px",
+        height:"250px",
+        display:"flex",
+        alingItems:"center",
+        justifyContent:"center"
+    };
+  
+    return (
+      <div style={estiloComponente}>
+        <img src={item.imagen} className="card-img-top image-fluid" alt={item.titulo} />
+      </div>
+    );
+  };
 
 const Item = ({item}) => {
     return (
         <div className="col-md-3 text-center">
-            <div className="card border-0">
+            <div className="card border-1 rounded-3 border-dark-subtle my-1">
                 <Link to={"/item/" + item.id} className="text-decoration-none text-dark">
-                    <img src={item.imagen} className="card-img-top image-fluid" alt={item.titulo} />
+                    <Imagenes item={item}/>
                     <div className="card-body">
                         <p className="card-text">{item.titulo}</p>
                     </div>
